763a6119e64de5872dffda4fad5456da0d0d68db
Tomáš Trval <trval@kajot.cz>
2023-04-03 10:17:22 +0200

    add: embedding api client with stored settings

--- muninn/parser/embeding.py
+++ muninn/parser/embeding.py
@@ -1,5 +1,5 @@
 import os
-from pprint import pprint
+from typing import Optional
 
 import openai
 from dotenv import load_dotenv
@@ -7,13 +7,41 @@ from dotenv import load_dotenv
 load_dotenv(override=True)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-ANALYZER_ADA = "text-embedding-ada-002"
-OPEN_API_KEY = os.environ.get("OPENAI_API_KEY")
-OPEN_API_ORG_KEY = os.environ.get("OPEN_API_ORG_KEY")
+DEFAULT_MODEL = "text-embedding-ada-002"
 
-input_to_analyze = "Valji owns Berserker sword"
-response = openai.Embedding.create(
-    input=input_to_analyze,
-    model=ANALYZER_ADA,
-)
-pprint(response["data"][-1]["embedding"])
+
+class Embedding:
+    """
+    OpenAI embedding calculator.
+
+    It stores setting to reduce passing model and key with each call of get embedding.
+    Configure once and call many times.
+    """
+
+    def __init__(
+            self,
+            model: Optional[str] = None,
+            api_key: Optional[str] = None,
+            org_key: Optional[str] = None,
+    ) -> None:
+        """"""
+        self.model = model or DEFAULT_MODEL
+        self.api_key = api_key or os.environ.get("OPENAI_API_KEY")
+        self.org_key = org_key or os.environ.get("OPEN_API_ORG_KEY")
+        if not self.api_key:
+            print("OPEN_API_KEY is not set, please set your key with environment variable")
+
+    def get(self, text: str) -> list[float]:
+        """
+        Calculate embedding vector with open api
+
+        :param text: text to calculate embedding for
+        :return: embedding vector representing text topics
+        """
+        response = openai.Embedding.create(
+            input=text,
+            model=self.model,
+            api_key=self.api_key,
+            organization=self.org_key,
+        )
+        return response["data"][-1]["embedding"]
