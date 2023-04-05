6801db1f92ab50c27ab3badc73cf2a627511c8ff
Tomáš Trval <trval@kajot.cz>
2023-04-05 10:14:29 +0200

    modify: include more typing

--- muninn/database/similarity_db.py
+++ muninn/database/similarity_db.py
@@ -2,11 +2,13 @@ import uuid
 from typing import Optional, Iterable
 
 import chromadb
+import chromadb.api
 import chromadb.utils.embedding_functions
-from chromadb.api.types import GetResult
+from chromadb.api.types import GetResult, Include
 from chromadb.config import Settings
 
 DEFAULT_COLLECTION = "human_context"
+DOCS_ONLY: Include = ["documents", ]
 
 __all__ = (
     "GetResult",
@@ -28,35 +30,41 @@ class VectorStorage:
     )
     """
     client: chromadb.Client
+    collection: chromadb.api.Collection
 
     def __init__(
             self,
             setting: chromadb.config.Settings,
             collection_name: Optional[str] = None,
     ) -> None:
-        self.client = chromadb.Client(setting)
-        self.collection = self.client.get_or_create_collection(
-            name=collection_name or DEFAULT_COLLECTION,
-        )
-
-    def get(self, *doc_ids) -> GetResult:
         """
-        Find document(s) by id(s)
+        Set up database client config.
+
+        :param setting: data container of storage settings server/local
+        :param collection_name: name for this memory
+        """
+        collection_name = collection_name or DEFAULT_COLLECTION
+        self.client = chromadb.Client(setting)
+        self.collection = self.client.get_or_create_collection(name=collection_name)
+
+    def get(self, doc_ids: str | list[str]) -> GetResult:
+        """
+        Find document(s) by id(s).
 
         :param doc_ids: document ids
         :return: matched documents
         """
         return self.collection.get(ids=doc_ids)
 
-    def query(self, embeddings: list[float], n_results=7) -> Iterable[str]:
+    def query(self, embeddings: list[float], n_results=17) -> Iterable[str]:
         """
-        FInd similar document
+        Find similar document.
 
         :param embeddings: search docs near this vector
         :param n_results: max limit returned doc number
         :return: documents
         """
-        result = self.collection.query(query_embeddings=embeddings, n_results=n_results, include=("documents",))
+        result = self.collection.query(query_embeddings=embeddings, n_results=n_results, include=DOCS_ONLY)
         return (d for d in result["documents"])
 
     def add(self, embedding: list[float], document: str) -> None:
