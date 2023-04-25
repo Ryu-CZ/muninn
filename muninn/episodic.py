2b2b48d3eb8e035c1b72fd83866d896bea775266
Tomáš Trval <trval@kajot.cz>
2023-04-25 15:06:35 +0200

    fix: parse numerical exteran and internals as string to be stored in sql

---
+++ muninn/episodic.py
@@ -0,0 +1,46 @@
+from dataclasses import dataclass
+
+from . import database, language
+
+
+@dataclass(slots=True)
+class AnalyzedText:
+    source: str  # source text
+    embedding: list[float]  # categorized text in vector space
+    episodic_id: str
+
+
+class Episodic:
+    """Episodic memory"""
+
+    def __init__(
+            self,
+            embedder: language.Embedding,
+            storage: database.VectorStorage,
+    ) -> None:
+        self.embedder = embedder
+        self.storage = storage
+
+    def analyze(self, text: str) -> list[float]:
+        """
+        Analyze and categorize text in vector space (embedding)
+
+        :param text: text to categorize
+        :return: calculated embedding
+        """
+        return self.embedder.get(text)
+
+    def add(self, text: str) -> AnalyzedText:
+        """
+        Remember this text to be found by similarity of context.
+
+        :param text: text to remember
+        :return: analyzed text
+        """
+        embedding = self.analyze(text)
+        episodic_id = self.storage.add(embedding=embedding, document=text)
+        return AnalyzedText(
+            source=text,
+            embedding=embedding,
+            episodic_id=episodic_id
+        )
